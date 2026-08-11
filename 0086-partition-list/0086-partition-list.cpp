303924d9edfe865160828bd8fcae18e33d8c7bd2
Noor Fatima <official.noorfatima.9@gmail.com>
2026-08-11 12:12:37 +0500

    Time: 0 ms (100%), Space: 15 MB (45.76%) - LeetHub

---
+++ 0086-partition-list/0086-partition-list.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    ListNode* partition(ListNode* head, int x) {
+
+        ListNode* lessDummy = new ListNode(0);
+        ListNode* greaterDummy = new ListNode(0);
+
+        ListNode* less = lessDummy;
+        ListNode* greater = greaterDummy;
+
+        while (head != nullptr) {
+
+            if (head->val < x) {
+                less->next = head;
+                less = less->next;
+            }
+            else {
+                greater->next = head;
+                greater = greater->next;
+            }
+
+            head = head->next;
+        }
+
+        // End the greater/equal list
+        greater->next = nullptr;
+
+        // Connect both lists
+        less->next = greaterDummy->next;
+
+        return lessDummy->next;
+    }
+};
