5730eaed83b497238d1ca5edc2a37c76f1f7eb2f
Noor Fatima <official.noorfatima.9@gmail.com>
2026-08-19 14:10:38 +0500

    Time: 15 ms (27.81%), Space: 17.8 MB (18.3%) - LeetHub

---
+++ 0139-word-break/0139-word-break.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool wordBreak(string s, vector<string>& wordDict) {
+
+        int n = s.length();
+
+        unordered_set<string> words(wordDict.begin(), wordDict.end());
+
+        vector<bool> dp(n + 1, false);
+
+        dp[0] = true;
+
+        for (int i = 1; i <= n; i++) {
+
+            for (int j = 0; j < i; j++) {
+
+                string word = s.substr(j, i - j);
+
+                if (dp[j] && words.count(word)) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+
+        return dp[n];
+    }
+};
